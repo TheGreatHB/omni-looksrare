180e0c07e5c9b8548b833fe4cdfc19e88201c8f1
TheGreatHB <hb@thegreathb.com>
2023-03-31 18:00:16 +0900

    Squashed commit of the following:
    commit 4fbb038386d3424b6c613e594cb0e804df3a7b89
    Merge: d89109a b0e263e
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Fri Mar 31 17:59:17 2023 +0900
    
        Merge branch 'working' of https://github.com/lz-asia/looksrare-integration into working
    
    commit d89109a1a82966f664dd2850e9db2260df0f2171
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Wed Mar 29 01:59:43 2023 +0900
    
        Add LooksRareIntegrator
    
        get gasUsed data from 1inch aggregator and analyze it
    
        update contract code
    
        almost done
    
        Update LooksRareIntergrator.sol
    
        update code
    
        housekeeping
    
        housekeeping for merging
    
    commit b0e263ec1cc9a993246196b4dfa106e31c9e0b99
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Fri Mar 31 17:55:21 2023 +0900
    
        housekeeping for merging
    
    commit 60d9ab693cafe74adb7f1299a80ed1ccc57879a0
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Fri Mar 31 17:51:40 2023 +0900
    
        housekeeping
    
    commit b95a89855450f053078ab901b4f2345fe2721694
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Fri Mar 31 17:48:42 2023 +0900
    
        update code
    
    commit 0cc95982bf2dbcb158ebb4e19ef58fb876e8adcf
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Fri Mar 31 17:26:03 2023 +0900
    
        Update LooksRareIntergrator.sol
    
    commit bcd7bcf906c2f02c23cd0b5f2c08c44dcb114d36
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Fri Mar 31 03:01:59 2023 +0900
    
        almost done
    
    commit 7b527bf44c528d126b5c09a9b1b08d2d3666b169
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Wed Mar 29 18:22:00 2023 +0900
    
        update contract code
    
    commit e32ae77fde120795bd51f51a0a8c8b34338d18ea
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Wed Mar 29 02:58:09 2023 +0900
    
        get gasUsed data from 1inch aggregator and analyze it
    
    commit 4c51ee9f17e1795313bf7596f6e9f2c35597cae5
    Author: TheGreatHB <hb@thegreathb.com>
    Date:   Wed Mar 29 01:59:43 2023 +0900
    
        Add LooksRareIntegrator

--- contracts/interfaces/IStargateFactory.sol
+++ contracts/interfaces/IStargateFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.7.6;
+pragma solidity >0.7.6;
 
 interface IStargateFactory {
     function getPool(uint256 poolId) external view returns (address);
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -203,6 +203,7 @@ const config: HardhatUserConfig = {
                 enabled: true,
                 runs: 200,
             },
+            viaIR: true,
         },
     },
     watcher: {
