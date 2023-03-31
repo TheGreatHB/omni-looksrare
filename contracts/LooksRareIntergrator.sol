37faee5ac1114934009f352aa4aa3217cd7b60da
TheGreatHB <hb@thegreathb.com>
2023-03-31 18:01:35 +0900

    Declare it's LooksRareIntergratorV1

--- contracts/LooksRareIntergrator.sol
+++ contracts/LooksRareIntergrator.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./interfaces/ILooksRareIntergrator.sol";
 
-contract LooksRareIntergrator is Ownable, ILooksRareIntergrator {
+contract LooksRareIntergratorV1 is Ownable, ILooksRareIntergrator {
     using SafeERC20 for IERC20;
     using Address for address;
 
